3e028c333612bd85fa471fbd3e34e7cb6d6e4aab
David Schmitt <david@dasz.at>
2013-01-14 17:02:16 +0100

    First test site.pp, configuring defaults for the puppetmaster.

---
+++ manifests/site.pp
@@ -0,0 +1,23 @@
+
+node 'puppetmaster.dasz.at' {
+  apt::source {
+    "wheezy-hetzner":
+      location          => "http://mirror.hetzner.de/debian/packages",
+      release           => "wheezy",
+      repos             => "main",
+      include_src       => false,
+      required_packages => "debian-archive-keyring",
+      key               => "55BE302B",
+      key_server        => "subkeys.pgp.net";
+
+    "wheezy-security":
+      location          => "http://security.debian.org/",
+      release           => "wheezy/updates",
+      repos             => "main",
+      include_src       => false,
+      required_packages => "debian-archive-keyring",
+      key               => "55BE302B",
+      key_server        => "subkeys.pgp.net";
+  }
+}
+
