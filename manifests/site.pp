c9d29f620b64a13987a72b1b7d81246025e7a041
David Schmitt <david@dasz.at>
2013-01-18 11:27:37 +0100

    Reference and use vcsrepo to checkout the puppet-secrets.
    This repo contains secrets that should not be published.

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,5 +33,38 @@ node 'puppetmaster.dasz.at' {
       key               => "55BE302B",
       key_server        => "subkeys.pgp.net";
   }
+
+  # of course, the following is not botstrappable, but after a manual intervention, it should lead to a stable, and migratable
+  # situation.
+  # for a key roll-over, the git server has to accept both the old and the new key until the puppetmaster has updated itself.
+  vcsrepo { "/srv/puppet/secrets":
+    ensure   => latest,
+    provider => git,
+    source   => "ssh://ccnet@dasz.at/srv/dasz/git/puppet-secrets.git",
+    owner    => root,
+    group    => root;
+  } -> file {
+    # vcsrepo does not manage the rights on the directory, so we have to.
+    # this leaves a little window of opportunity where the secrets are accessible, after 
+    # cloning the repository. Since this should only happen when the puppetmaster is 
+    # re-imaged, I do not believe this to be a problem.
+    "/srv/puppet/secrets":
+      ensure => directory,
+      mode   => 0700,
+      owner  => root,
+      group  => root;
+
+    "/root/.ssh":
+      ensure => directory,
+      mode   => 0700,
+      owner  => root,
+      group  => root;
+
+    "/root/.ssh/id_rsa":
+      source => "/srv/puppet/secrets/puppetmaster/id_rsa",
+      mode   => 0600,
+      owner  => root,
+      group  => root;
+  }
 }
 
