640cb78eaaa09b8e1b6fd1d91ba541f90aaf4136
David Schmitt <david@dasz.at>
2013-04-07 14:30:20 +0200

    veewee: combine all incantations into a script

---
+++ vagrant/force-build-wheezy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+rbenv shell 1.9.2-p320 # for the local shell only
+rbenv exec bundle install
+export VEEWEE_PURE=yes
+nice rbenv exec bundle exec vagrant basebox define --force 'Debian-7.0.0-amd64' 'Debian-7.0-rc1-amd64-netboot'
+nice rbenv exec bundle exec vagrant basebox build --force 'Debian-7.0.0-amd64'
+rbenv exec bundle exec vagrant basebox validate 'Debian-7.0.0-amd64'
+nice rbenv exec bundle exec vagrant basebox export --force 'Debian-7.0.0-amd64'
+nice vagrant box add --force 'Debian-7.0.0-amd64' 'Debian-7.0.0-amd64.box'
+
