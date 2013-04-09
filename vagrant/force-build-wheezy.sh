1be92b439dd9e29e23d1b12c033775605d111212
David Schmitt <david@dasz.at>
2013-04-09 19:21:29 +0200

    vagrant: don't use PURE for now

--- vagrant/force-build-wheezy.sh
+++ vagrant/force-build-wheezy.sh
@@ -4,7 +4,10 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 rbenv shell 1.9.2-p320 # for the local shell only
 rbenv exec bundle install
-export VEEWEE_PURE=yes
+
+# Sadly upgrade from puppet/wheezy to puppet/puppetlabs doesn't work out of the box
+# export VEEWEE_PURE=yes
+
 nice rbenv exec bundle exec vagrant basebox define --force 'Debian-7.0.0-amd64' 'Debian-7.0-rc1-amd64-netboot'
 nice rbenv exec bundle exec vagrant basebox build --force 'Debian-7.0.0-amd64'
 rbenv exec bundle exec vagrant basebox validate 'Debian-7.0.0-amd64'
