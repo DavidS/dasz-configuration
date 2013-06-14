74924105494906194c078a9538367820930f5220
David Schmitt <david@dasz.at>
2013-06-14 08:24:34 +0200

    jenkins: configure jenkins' repo

--- manifests/nodes/jenkins.dasz.at.pp
+++ manifests/nodes/jenkins.dasz.at.pp
@@ -1,3 +1,10 @@
 node 'jenkins.dasz.at' {
   class { 'dasz::defaults': location => tech21; }
+  apt::repository { 'jenkins':
+    url        => 'http://pkg.jenkins-ci.org/debian',
+    distro     => '',
+    repository => 'binary/',
+    key        => 'D50582E6',
+    key_url    => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key';
+  }
 }
