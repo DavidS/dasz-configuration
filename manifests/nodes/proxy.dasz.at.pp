4e87f2c01879ab4df26cfbe4a195f60e8e8d7ba6
David Schmitt <david@dasz.at>
2013-06-20 18:17:13 +0200

    move another munin port

--- manifests/nodes/proxy.dasz.at.pp
+++ manifests/nodes/proxy.dasz.at.pp
@@ -1,6 +1,7 @@
 node 'proxy.dasz.at' {
   class { 'dasz::defaults':
-    location => hetzner,
-    ssh_port => 2201;
+    location   => hetzner,
+    ssh_port   => 2201,
+    munin_port => 4951;
   }
 }
