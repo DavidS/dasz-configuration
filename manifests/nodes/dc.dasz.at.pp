0d9cfbb67888ea4d6b45f9a155801a12c83d856d
David Schmitt <david@dasz.at>
2013-06-26 17:04:55 +0200

    manually configure munin plugins

--- manifests/nodes/dc.dasz.at.pp
+++ manifests/nodes/dc.dasz.at.pp
@@ -3,4 +3,14 @@ node 'dc.dasz.at' {
     location    => tech21,
     admin_users => false; # collides with local ldap setup
   }
+  file {
+    '/etc/munin/plugins/ttys_temp':
+      ensure => link,
+      target => '/etc/munin/ttys_temp',
+      notify => Service['munin-node'];
+    '/etc/munin/plugins/sensors':
+      ensure => link,
+      target => '/etc/munin/sensors',
+      notify => Service['munin-node'];
+  }
 }
