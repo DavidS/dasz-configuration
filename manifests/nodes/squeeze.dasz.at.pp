e10c04fb71fff1bf9ea74f148d283bdd1cd7df70
David Schmitt <david@dasz.at>
2013-06-26 17:57:45 +0200

    squeeze: configure zetbox exception graph

--- manifests/nodes/squeeze.dasz.at.pp
+++ manifests/nodes/squeeze.dasz.at.pp
@@ -1,3 +1,8 @@
 node 'squeeze.dasz.at' {
   class { 'dasz::defaults': location => tech21; }
+  munin::plugin { 'zetbox_exceptions_ini50':
+    source => 'dasz/munin/zetbox_exceptions_',
+    source_config => 'dasz/munin/zetbox_exceptions_ini50',
+    linkplugins => true;
+  }
 }
