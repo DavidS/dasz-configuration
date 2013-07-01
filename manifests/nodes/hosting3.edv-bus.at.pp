c533daab661e27420e40d9b5467273b06cc9fcfe
David Schmitt <david@dasz.at>
2013-07-01 16:46:37 +0200

    fix

--- manifests/nodes/hosting3.edv-bus.at.pp
+++ manifests/nodes/hosting3.edv-bus.at.pp
@@ -1,7 +1,6 @@
 # tmp stub
 node 'hosting3.edv-bus.at' {
   class { 'dasz::defaults':
-    location    => hetzner,
-    admin_users => false; # collides with existing users
+    location    => hetzner;
   }
 }
