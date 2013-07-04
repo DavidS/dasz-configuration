e5c5013265e4fe4212c01e6586e541fcaee794bb
David Schmitt <david@dasz.at>
2013-07-04 15:52:08 +0200

    Configure openvpn server on hetz3

--- manifests/nodes/hetz3.black.co.at.pp
+++ manifests/nodes/hetz3.black.co.at.pp
@@ -4,10 +4,16 @@ node 'hetz3.black.co.at' {
     ssh_port => 2200;
   }
 
+  class {
+    'libvirt':
+    ;
+
+    'openvpn':
+    ;
+  }
+
   # foreman virt host
   $home = '/var/lib/foreman-mgr'
-
-  class { 'libvirt': }
 
   user { 'foreman-mgr':
     ensure     => present,
@@ -30,5 +36,38 @@ node 'hetz3.black.co.at' {
       owner  => 'foreman-mgr',
       group  => 'foreman-mgr',
       mode   => 0600;
+
+    "/etc/openvpn/dasz-ca":
+      ensure => directory,
+      mode   => '0700',
+      owner  => root,
+      group  => root;
+
+    "/etc/openvpn/dasz-ca/keys":
+      ensure  => directory,
+      mode    => '0700',
+      owner   => root,
+      group   => root,
+      source  => 'puppet:///secrets/openvpn_ca/dasz-ca/keys',
+      ignore  => ['*.key', '*.csr'],
+      recurse => true,
+      force   => true,
+      purge   => true;
+  }
+
+  openvpn::tunnel {
+    'dasz-lan':
+      port     => 1197,
+      template => 'site/hosting3.edv-bus.at/openvpn_dasz-lan.conf.erb';
+
+    'dasz-lan-david-nb':
+      port     => 1198,
+      server   => '10.254.0.5 10.254.0.6',
+      template => 'site/hosting3.edv-bus.at/openvpn_dasz-lan-nb.conf.erb';
+
+    'dasz-lan-arthur-nb':
+      port     => 1199,
+      server   => '10.254.0.9 10.254.0.10',
+      template => 'site/hosting3.edv-bus.at/openvpn_dasz-lan-nb.conf.erb';
   }
 }
