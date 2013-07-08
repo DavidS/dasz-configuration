e2b8bf5d712bc8dcea60b4d4b137eb5139bd5706
David Schmitt <david@dasz.at>
2013-07-08 14:18:23 +0200

    deploy nginx reverse proxy

--- manifests/nodes/hetz3.black.co.at.pp
+++ manifests/nodes/hetz3.black.co.at.pp
@@ -54,4 +54,47 @@ node 'hetz3.black.co.at' {
   }
 
   site::ovpn_keys { ['dasz', 'maria']: }
+
+  nginx::vhost {
+    'default':
+      docroot        => 'none',
+      create_docroot => false,
+      template       => "site/${::fqdn}/nginx-default.site.erb";
+
+    'office.dasz.at':
+      docroot        => 'none',
+      create_docroot => false,
+      template       => "site/${::fqdn}/nginx-office.dasz.at.site.erb";
+  }
+
+  # required so that nginx finds the upstrem server
+  host { "office":
+    ip     => '10.0.0.221',
+    notify => Service['nginx'];
+  }
+
+  file {
+    '/etc/nginx/certs':
+      ensure => directory,
+      mode   => 0750,
+      owner  => root,
+      group  => 'www-data',
+      notify => Service['nginx'];
+
+    '/etc/nginx/certs/office.dasz.at.key':
+      ensure => present,
+      source => 'puppet:///secrets/ssl/office.dasz.at.key',
+      mode   => 0440,
+      owner  => root,
+      group  => 'www-data',
+      notify => Service['nginx'];
+
+    '/etc/nginx/certs/office.dasz.at.bundle.crt':
+      ensure => present,
+      source => 'puppet:///secrets/ssl/office.dasz.at.bundle.crt',
+      mode   => 0440,
+      owner  => root,
+      group  => 'www-data',
+      notify => Service['nginx'];
+  }
 }
