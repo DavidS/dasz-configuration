10990f0c29b1ae2518682f4b8173d4d3adfa42f5
David Schmitt <david@dasz.at>
2013-07-09 07:37:06 +0200

    fw-schmidg2: force primary ip to vpn

--- manifests/nodes/fw-schmidg2.edv-bus.at.pp
+++ manifests/nodes/fw-schmidg2.edv-bus.at.pp
@@ -1,6 +1,7 @@
 node 'fw-schmidg2.edv-bus.at' {
   class { 'dasz::defaults':
     location         => 'Schmidg',
+    primary_ip       => '192.168.0.2', # only reachable via ovpn-maria
     force_nullmailer => true;
   }
 }
