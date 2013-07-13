b6b48d76d915a6bf4004b5ef9035b713258ed1e4
David Schmitt <david@dasz.at>
2013-07-13 17:50:45 +0200

    hosting3: activate systemd

--- manifests/nodes/hosting3.edv-bus.at.pp
+++ manifests/nodes/hosting3.edv-bus.at.pp
@@ -1,6 +1,20 @@
-# tmp stub
 node 'hosting3.edv-bus.at' {
-  class { 'dasz::defaults':
-    location    => hetzner;
+  class { 'dasz::defaults': location => hetzner; }
+
+  package { 'systemd': ensure => installed; }
+
+  file { "/etc/default/grub":
+    ensure  => present,
+    source  => 'puppet:///modules/dasz/systemd-grub.conf',
+    mode    => 0644,
+    owner   => root,
+    group   => root,
+    require => Package['systemd'],
+    notify  => Exec['update-grub'];
+  }
+
+  exec { 'update-grub':
+    refreshonly => true,
+    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin';
   }
 }
