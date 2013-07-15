7785b4c762f0abd9939f8edc7960ade2bcbf29c7
David Schmitt <david@dasz.at>
2013-07-15 08:56:35 +0200

    hosting3: do not collide with hosting ssh

--- manifests/nodes/hosting3.edv-bus.at.pp
+++ manifests/nodes/hosting3.edv-bus.at.pp
@@ -1,5 +1,8 @@
 node 'hosting3.edv-bus.at' {
-  class { 'dasz::defaults': location => hetzner; }
+  class { 'dasz::defaults':
+    location => hetzner,
+    ssh_port => 2200; # do not collide with hosting ssh
+  }
 
   package { 'systemd': ensure => installed; }
 
