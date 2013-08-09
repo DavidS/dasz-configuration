c99360b59a9b2fe6c4417568dc9caa2bec84a130
David Schmitt <david@dasz.at>
2013-08-09 13:17:54 +0200

    nodes: update kvmhost2 to wheezy; start foreman-proxy installation

--- manifests/nodes/kvmhost2.dasz.at.pp
+++ manifests/nodes/kvmhost2.dasz.at.pp
@@ -1,8 +1,24 @@
 node 'kvmhost2.dasz.at' {
-  class { 'dasz::defaults':
-    location          => tech21,
-    ssh_address       => '10.0.0.192',
-    munin_smart_disks => ['sda', 'sdb', 'sdc'],
-    force_nullmailer  => true;
+  class {
+    'dasz::defaults':
+      location          => tech21,
+      ssh_address       => '10.0.0.192',
+      munin_smart_disks => ['sda', 'sdb', 'sdc'],
+      force_nullmailer  => true;
+
+    'libvirt':
+    ;
+  }
+
+  # foreman virt host
+  $home = '/var/lib/foreman-mgr'
+
+  user { 'foreman-mgr':
+    ensure     => present,
+    system     => true,
+    home       => $home,
+    managehome => true,
+    groups     => ['libvirt'],
+    require    => Package['libvirt'];
   }
 }
