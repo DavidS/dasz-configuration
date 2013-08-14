06ac71589d96c69ea934d751e6d812a1ab895760
David Schmitt <david@dasz.at>
2013-08-14 12:28:37 +0200

    Remove obsolete container; update exceptions monitoring to new setup

--- manifests/nodes/jenkins.dasz.at.pp
+++ manifests/nodes/jenkins.dasz.at.pp
@@ -15,4 +15,11 @@ node 'jenkins.dasz.at' {
     key        => 'D50582E6',
     key_url    => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key';
   }
+
+  munin::plugin { 'zetbox_exceptions_ini50':
+    source        => 'puppet:///modules/dasz/munin/zetbox_exceptions_',
+    config_source => 'puppet:///modules/dasz/munin/zetbox_exceptions_ini50';
+  }
+
+  package { 'postgresql-client-8.4': ensure => present; }
 }
--- manifests/nodes/mvc3.dasz.at.pp
+++
@@ -1,6 +0,0 @@
-node 'mvc3.dasz.at' {
-  class { 'dasz::defaults':
-    location         => tech21,
-    force_nullmailer => true;
-  }
-}
