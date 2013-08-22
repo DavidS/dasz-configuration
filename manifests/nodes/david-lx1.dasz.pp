571a4537f3159b9c3b4d40f4a33de7e8db1bd684
David Schmitt <david@dasz.at>
2013-08-22 13:41:29 +0200

    workstation: Add sid sources

--- manifests/nodes/david-lx1.dasz.pp
+++ manifests/nodes/david-lx1.dasz.pp
@@ -6,9 +6,16 @@ node 'david-lx1.dasz' {
     force_nullmailer  => true;
   }
 
-  apt::repository { "zetbox":
-    url        => "http://kvmhost.dasz/debian",
-    distro     => zetbox,
-    repository => "main";
+  apt::repository {
+    "zetbox":
+      url        => "http://kvmhost.dasz/debian",
+      distro     => zetbox,
+      repository => "main";
+
+    "sid-sources":
+      url        => "http://kvmhost.dasz:3142/debian",
+      distro     => sid,
+      repository => "main",
+      src_repo   => true;
   }
 }
