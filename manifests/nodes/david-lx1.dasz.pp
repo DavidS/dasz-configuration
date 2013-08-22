9b15f531a41b61296d3ca3469ef253f8fc017507
David Schmitt <david@dasz.at>
2013-08-22 11:00:45 +0200

    worksattion: add zetbox repo

--- manifests/nodes/david-lx1.dasz.pp
+++ manifests/nodes/david-lx1.dasz.pp
@@ -5,4 +5,10 @@ node 'david-lx1.dasz' {
     munin_smart_disks => ['sda', 'sdb'],
     force_nullmailer  => true;
   }
+
+  apt::repository { "zetbox":
+    url        => "http://kvmhost.dasz/debian",
+    distro     => zetbox,
+    repository => "main";
+  }
 }
