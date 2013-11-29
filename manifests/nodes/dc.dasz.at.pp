8ccd50ff399d1e1306fe85f2f08764b1d30e50ee
David Schmitt <david@dasz.at>
2013-11-29 09:41:52 +0100

    dc: Add tftp

--- manifests/nodes/dc.dasz.at.pp
+++ manifests/nodes/dc.dasz.at.pp
@@ -1,9 +1,13 @@
 node 'dc.dasz.at' {
-  class { 'dasz::defaults':
-    location          => tech21,
-    admin_users       => false, # collides with local ldap setup
-    munin_smart_disks => ['sda', 'sdb', 'sdc', 'sdd'], # backup disks may not be well-checkable
-    force_nullmailer  => true;
+  class {
+    'dasz::defaults':
+      location          => tech21,
+      admin_users       => false, # collides with local ldap setup
+      munin_smart_disks => ['sda', 'sdb', 'sdc', 'sdd'], # backup disks may not be well-checkable
+      force_nullmailer  => true;
+
+    'tftp':
+    ;
   }
 
   munin::plugin {
