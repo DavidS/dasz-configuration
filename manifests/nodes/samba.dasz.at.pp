93995590ae1aedc1c085a39fc3930c1515079d99
David Schmitt <david@dasz.at>
2013-11-29 12:24:06 +0100

    samba: configure backuppc

--- manifests/nodes/samba.dasz.at.pp
+++ manifests/nodes/samba.dasz.at.pp
@@ -5,4 +5,18 @@ node 'samba.dasz.at' {
     munin_smart_disks => ['sda', 'sdb', 'sdc', 'sdd'], # backup disks may not be well-checkable
     force_nullmailer  => true;
   }
+
+  package { 'backuppc': ensure => installed; }
+
+  file { "/etc/backuppc":
+    source  => "puppet:///secrets/backuppc",
+    recurse => true,
+    require => Package['backuppc'],
+    notify  => Service['backuppc'];
+  }
+
+  service { 'backuppc':
+    ensure => 'running',
+    enable => true;
+  }
 }
