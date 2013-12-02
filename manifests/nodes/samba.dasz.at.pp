2cb7aef890e571975b109e7b4b95f98cd8d180e3
David Schmitt <david@dasz.at>
2013-12-02 10:05:43 +0100

    samba: enable systemd

--- manifests/nodes/samba.dasz.at.pp
+++ manifests/nodes/samba.dasz.at.pp
@@ -1,9 +1,13 @@
 node 'samba.dasz.at' {
-  class { 'dasz::defaults':
-    location          => tech21,
-    admin_users       => false, # collides with local ldap setup
-    munin_smart_disks => ['sda', 'sdb', 'sdc', 'sdd'], # backup disks may not be well-checkable
-    force_nullmailer  => true;
+  class {
+    'dasz::defaults':
+      location          => tech21,
+      admin_users       => false, # collides with local ldap setup
+      munin_smart_disks => ['sda', 'sdb', 'sdc', 'sdd'], # backup disks may not be well-checkable
+      force_nullmailer  => true;
+
+    'dasz::snips::systemd':
+    ;
   }
 
   package { 'backuppc': ensure => installed; }
