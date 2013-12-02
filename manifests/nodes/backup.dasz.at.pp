757d5d95bb7938ef13ba8743c1496ffd024eebdd
David Schmitt <david@dasz.at>
2013-12-02 16:17:52 +0100

    site: Rename backup host

--- manifests/nodes/backup.dasz.at.pp
+++ manifests/nodes/backup.dasz.at.pp
@@ -1,8 +1,7 @@
-node 'samba.dasz.at' {
+node 'backup.dasz.at' {
   class {
     'dasz::defaults':
       location          => tech21,
-      admin_users       => false, # collides with local ldap setup
       munin_smart_disks => ['sda', 'sdb', 'sdc', 'sdd'], # backup disks may not be well-checkable
       force_nullmailer  => true;
 
