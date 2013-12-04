49e6ca4ef153fef8617e9e4d5336e7c32fe382ac
David Schmitt <david@dasz.at>
2013-12-04 11:40:40 +0100

    samba: avoid using office line for upstream

--- manifests/nodes/samba.dasz.at.pp
+++ manifests/nodes/samba.dasz.at.pp
@@ -1,12 +1,30 @@
 node 'samba.dasz.at' {
   class {
     'dasz::defaults':
-      location          => tech21,
+      location          => hetzner,
       admin_users       => false, # collides with local ldap setup
       munin_smart_disks => ['sda'],
       force_nullmailer  => true;
 
     'dasz::snips::systemd':
     ;
+  }
+
+  # eth0 is openvpn to office
+  # eth1 is nat to hetzner
+  # avoid using office line for upstream
+  file { "/etc/dhcp/dhclient-enter-hooks.d/no-default-route-via-tech21":
+    content => "if [ \"\$interface\" = \"eth0\" ]; then
+  case \$reason in
+    BOUND|RENEW|REBIND|REBOOT)
+      unset new_routers
+      ;;
+  esac
+fi
+
+",
+    mode    => 0644,
+    owner   => root,
+    group   => root;
   }
 }
