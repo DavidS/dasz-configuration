ba531a2e5eccdcc4623e0ea970fb5d3d0cbf0769
David Schmitt <david@dasz.at>
2013-12-04 11:27:52 +0100

    samba: update to testing; add systemd

--- manifests/nodes/samba.dasz.at.pp
+++ manifests/nodes/samba.dasz.at.pp
@@ -1,8 +1,12 @@
 node 'samba.dasz.at' {
-  class { 'dasz::defaults':
-    location          => tech21,
-    admin_users       => false, # collides with local ldap setup
-    munin_smart_disks => ['sda'],
-    force_nullmailer  => true;
+  class {
+    'dasz::defaults':
+      location          => tech21,
+      admin_users       => false, # collides with local ldap setup
+      munin_smart_disks => ['sda'],
+      force_nullmailer  => true;
+
+    'dasz::snips::systemd':
+    ;
   }
 }
