e8fefd2fe2394a42d9726d943509bf1a460dd01b
David Schmitt <david@dasz.at>
2013-12-17 09:35:44 +0100

    Stash local changes

--- vagrant/force-build-wheezy.sh
+++ vagrant/force-build-wheezy.sh
@@ -5,9 +5,9 @@ eval "$(rbenv init -)"
 rbenv shell 1.9.2-p320 # for the local shell only
 rbenv exec bundle install
 
-nice rbenv exec bundle exec vagrant basebox define --force 'Debian-7.1.0-amd64' 'Debian-7.1.0-amd64-netboot'
-nice rbenv exec bundle exec vagrant basebox build --force 'Debian-7.1.0-amd64'
-rbenv exec bundle exec vagrant basebox validate 'Debian-7.1.0-amd64'
-nice rbenv exec bundle exec vagrant basebox export --force 'Debian-7.1.0-amd64'
+nice rbenv exec bundle exec veewee vbox define --force 'Debian-7.1.0-amd64' 'Debian-7.1.0-amd64-netboot'
+nice rbenv exec bundle exec veewee vbox build --force 'Debian-7.1.0-amd64'
+rbenv exec bundle exec veewee vbox validate 'Debian-7.1.0-amd64'
+nice rbenv exec bundle exec veewee vbox export --force 'Debian-7.1.0-amd64'
 nice vagrant box add --force 'Debian-7.1.0-amd64' 'Debian-7.1.0-amd64.box'
 
