1d660dbc9edc443b6d6dc16b6c5ff9c1fe4fd452
David Schmitt <david@dasz.at>
2014-01-08 15:23:15 +0100

    Add hosting::customers

--- manifests/nodes/hosting3.edv-bus.at.pp
+++ manifests/nodes/hosting3.edv-bus.at.pp
@@ -1,10 +1,22 @@
+# placeholder
+define hosting::customer () {
+  file { "/data/srv/${name}":
+    ensure => directory,
+    noop   => true;
+  }
+}
+
 node 'hosting3.edv-bus.at' {
   class {
     'dasz::defaults':
       location => hetzner,
       ssh_port => 2200; # do not collide with hosting ssh
 
+
+
     'dasz::snips::systemd':
     ;
   }
+
+  create_resources("hosting::customer", loadyaml("/srv/puppet/secrets/hosting/customers.yaml"))
 }
