3da57d4060e1dec86177b9b05a56f70979da61cd
David Schmitt <david@dasz.at>
2014-01-08 21:44:52 +0100

    Extract debian version into variable

--- vagrant/force-build-wheezy.sh
+++ vagrant/force-build-wheezy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
 
+VERSION="Debian-7.2.0-amd64"
+
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 rbenv shell 1.9.2-p320 # for the local shell only
 rbenv exec bundle install
 
-nice rbenv exec bundle exec veewee vbox define --force 'Debian-7.1.0-amd64' 'Debian-7.1.0-amd64-netboot'
-nice rbenv exec bundle exec veewee vbox build --force 'Debian-7.1.0-amd64'
-rbenv exec bundle exec veewee vbox validate 'Debian-7.1.0-amd64'
-nice rbenv exec bundle exec veewee vbox export --force 'Debian-7.1.0-amd64'
-nice vagrant box add --force 'Debian-7.1.0-amd64' 'Debian-7.1.0-amd64.box'
+nice rbenv exec bundle exec veewee vbox define --force "$VERSION" "$VERSION-netboot"
+nice rbenv exec bundle exec veewee vbox build --force "$VERSION"
+rbenv exec bundle exec veewee vbox validate "$VERSION"
+nice rbenv exec bundle exec veewee vbox export --force "$VERSION"
+nice vagrant box add --force "$VERSION" "$VERSION.box"
 
