b1258539b52b26b7f201ff87d045ee4fc71d0d81
David Schmitt <david@dasz.at>
2014-01-13 15:54:47 +0100

    dasz: remove more unused stuff

--- modules/hosting/manifests/init.pp
+++ modules/hosting/manifests/init.pp
@@ -11,30 +11,6 @@
 # Sample Usage:
 #
 class hosting {
-  #  file { "/lib/systemd/system-generators/hosting-apps-generator":
-  #    source => 'puppet:///modules/hosting/hosting-apps-generator',
-  #    mode   => 0755,
-  #    owner  => root,
-  #    group  => root,
-  #    notify => Exec['systemd-reload'];
-  #  }
-  #
-  #  file {
-  #    "/etc/systemd/user/dbus.socket":
-  #      source => 'puppet:///modules/hosting/dbus.socket',
-  #      mode   => 0644,
-  #      owner  => root,
-  #      group  => root,
-  #      notify => Exec['systemd-reload'];
-  #
-  #    "/etc/systemd/user/dbus.service":
-  #      source => 'puppet:///modules/hosting/dbus.service',
-  #      mode   => 0644,
-  #      owner  => root,
-  #      group  => root,
-  #      notify => Exec['systemd-reload'];
-  #  }
-
   # use mono3
   apt::repository { "zetbox":
     url        => "http://kvmhost.dasz/debian",
