b3a2869f06c8048b506cbdb789b27edf4e587a95
David Schmitt <david@dasz.at>
2014-01-24 12:54:20 +0100

    veewee: update and fix; add and use systemd capability

--- vagrant/force-build-wheezy.sh
+++ vagrant/force-build-wheezy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-VERSION="Debian-7.2.0-amd64"
+VERSION="Debian-7.3.0-amd64"
+export VEEWEE_USE_SYSTEMD=yes
 
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
@@ -10,6 +11,7 @@ rbenv exec bundle install
 nice rbenv exec bundle exec veewee vbox define --force "$VERSION" "$VERSION-netboot"
 nice rbenv exec bundle exec veewee vbox build --force "$VERSION"
 rbenv exec bundle exec veewee vbox validate "$VERSION"
+sleep 1 # yes, really
 nice rbenv exec bundle exec veewee vbox export --force "$VERSION"
 nice vagrant box add --force "$VERSION" "$VERSION.box"
 
