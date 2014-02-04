a6f2757cd784b92173b30587ddd7d27e765a4103
David Schmitt <david@dasz.at>
2014-02-04 11:04:24 +0100

    hosting: pass roundcube_db_password from secrets

--- manifests/nodes/hosting3.edv-bus.at.pp
+++ manifests/nodes/hosting3.edv-bus.at.pp
@@ -11,12 +11,13 @@ node 'hosting3.edv-bus.at' {
     ;
 
     'hosting':
-      primary_fqdn      => 'hosting.edv-bus.at',
-      primary_ns_name   => 'ns1.edv-bus.at',
-      secondary_ns_name => 'ns2.edv-bus.at',
-      primary_mx_name   => 'mail.edv-bus.at',
-      hosting_ipaddress => $::ipaddress,
-      hostmaster        => 'hostmaster.edv-bus.at';
+      primary_fqdn          => 'hosting.edv-bus.at',
+      primary_ns_name       => 'ns1.edv-bus.at',
+      secondary_ns_name     => 'ns2.edv-bus.at',
+      primary_mx_name       => 'mail.edv-bus.at',
+      hosting_ipaddress     => $::ipaddress,
+      hostmaster            => 'hostmaster.edv-bus.at',
+      roundcube_db_password => file("/srv/puppet/secrets/${::fqdn}/roundcube_db.password");
   }
 
   $customers = loadyaml("/srv/puppet/secrets/hosting/customers.yaml")
