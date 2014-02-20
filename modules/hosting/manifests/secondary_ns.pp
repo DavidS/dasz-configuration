29e3d01a7fde288c2f8d6249ba70ca8f76a250f9
David Schmitt <david@dasz.at>
2014-02-20 16:26:22 +0100

    hosting/secondary_ns: try to improve reliability of bind9 restarting
    This service file is cribbed from sid's bind9 packaging

--- modules/hosting/manifests/secondary_ns.pp
+++ modules/hosting/manifests/secondary_ns.pp
@@ -8,6 +8,17 @@ class hosting::secondary_ns {
     require => Class['bind::installation'],
     notify  => Class['bind::service'];
   }
- 
+
   Concat::Fragment <<| tag == 'hosting::domain::slave' |>>
+
+  # bind service file required for reliable restarting
+  file { "/etc/systemd/system/bind9.service":
+    source => "puppet:///modules/hosting/systemd.bind9.service",
+    mode   => 0644,
+    owner  => root,
+    group  => root,
+    notify => [Exec['systemd-reload'], Class['bind::service']];
+  }
+  # require that systemd reloads before updating the bind9 service
+  Exec['systemd-reload'] -> Class['bind::service']
 }
