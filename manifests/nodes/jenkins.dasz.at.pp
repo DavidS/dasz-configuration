1e13f3fec5db1c069a0f2f77bf6d97e0c359ad23
David Schmitt <david@dasz.at>
2014-02-28 08:14:56 +0100

    jenkins: move apt repo to snippet

--- manifests/nodes/jenkins.dasz.at.pp
+++ manifests/nodes/jenkins.dasz.at.pp
@@ -6,14 +6,7 @@ node 'jenkins.dasz.at' {
 
     'dasz::snips::jenkins':
       url => 'http://jenkins:8080';
-  }
 
-  apt::repository { 'jenkins':
-    url        => 'http://pkg.jenkins-ci.org/debian',
-    distro     => '',
-    repository => 'binary/',
-    key        => 'D50582E6',
-    key_url    => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key';
   }
 
   dasz::zetbox::monitor {
--- modules/dasz/manifests/snips/jenkins.pp
+++ modules/dasz/manifests/snips/jenkins.pp
@@ -1,11 +1,19 @@
 # configures the munin plugin for the specified url
-class dasz::snips::jenkins($url) {
-  package { 'libwww-perl': ensure => present; }
+class dasz::snips::jenkins ($url) {
+  package { ['jenkins', 'libwww-perl',]: ensure => present; }
 
   munin::plugin { 'jenkins_status':
     ensure         => 'present',
     source         => 'puppet:///modules/dasz/munin/jenkins_status',
     config_content => "[jenkins_*]\nenv.url $url\n";
+  }
+
+  apt::repository { 'jenkins':
+    url        => 'http://pkg.jenkins-ci.org/debian',
+    distro     => '',
+    repository => 'binary/',
+    key        => 'D50582E6',
+    key_url    => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key';
   }
 }
 
