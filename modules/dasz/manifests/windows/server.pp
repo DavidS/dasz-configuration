9e6e2aea64a0079e1403609a48f70b019ec2e3d7
David Schmitt <david@dasz.at>
2014-04-28 08:40:42 +0200

    dasz/windows: demote windows servers to business-hrs notification

--- modules/dasz/manifests/windows/server.pp
+++ modules/dasz/manifests/windows/server.pp
@@ -1,4 +1,7 @@
 # a windows server
 class dasz::windows::server {
-  class { 'dasz::windows': nagios_notifications => true; }
+  class { 'dasz::windows':
+    nagios_notifications       => true,
+    nagios_notification_period => 'business-hrs';
+  }
 }
