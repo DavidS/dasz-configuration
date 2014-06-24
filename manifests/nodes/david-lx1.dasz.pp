426c1c55cdb3fa90c00d65422beef85f9780604f
David Schmitt <david@dasz.at>
2014-06-24 20:05:15 +0200

    david-lx1: add dasz-bridge config; libvirt; remove unused repos

--- manifests/nodes/david-lx1.dasz.pp
+++ manifests/nodes/david-lx1.dasz.pp
@@ -4,18 +4,30 @@ node 'david-lx1.dasz' {
     apt_dater_manager => false, # temp fix
     munin_smart_disks => ['sda', 'sdb'],
     force_nullmailer  => true;
+
+    'libvirt':
+    ;
   }
 
-  apt::repository {
-    "zetbox":
-      url        => "http://kvmhost.dasz/debian",
-      distro     => zetbox,
-      repository => "main";
+#  apt::repository {
+#    "zetbox":
+#      url        => "http://kvmhost.dasz/debian",
+#      distro     => zetbox,
+#      repository => "main";
+#
+#    "sid-sources":
+#      url        => "http://kvmhost.dasz:3142/debian",
+#      distro     => sid,
+#      repository => "main",
+#      src_repo   => true;
+#  }
 
-    "sid-sources":
-      url        => "http://kvmhost.dasz:3142/debian",
-      distro     => sid,
-      repository => "main",
-      src_repo   => true;
+  openvpn::tunnel { 'dasz-bridge':
+    port     => 1194,
+    proto    => 'udp',
+    server   => '10.254.0.2 10.254.0.1',
+    template => "site/${::fqdn}/openvpn_dasz-bridge.conf.erb";
   }
+
+  site::ovpn_keys { 'dasz': }
 }
