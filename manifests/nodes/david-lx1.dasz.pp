b78a4e0966cf7fe11541d719907a5ed3b6ba9dfe
David Schmitt <david@dasz.at>
2014-06-24 20:02:17 +0200

    david-lx1: temp workaround

--- manifests/nodes/david-lx1.dasz.pp
+++ manifests/nodes/david-lx1.dasz.pp
@@ -1,7 +1,7 @@
 node 'david-lx1.dasz' {
   class { 'dasz::defaults':
     location          => at,
-    apt_dater_manager => true,
+    apt_dater_manager => false, # temp fix
     munin_smart_disks => ['sda', 'sdb'],
     force_nullmailer  => true;
   }
