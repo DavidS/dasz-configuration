26fb7d8e3ae697340082305e7237001d4177573e
David Schmitt <david@dasz.at>
2014-06-26 21:38:14 +0200

    nodes/david-lx1: remove apt-dater fix

--- manifests/nodes/david-lx1.dasz.pp
+++ manifests/nodes/david-lx1.dasz.pp
@@ -1,9 +1,10 @@
 node 'david-lx1.dasz' {
-  class { 'dasz::defaults':
-    location          => at,
-    apt_dater_manager => false, # temp fix
-    munin_smart_disks => ['sda', 'sdb'],
-    force_nullmailer  => true;
+  class {
+    'dasz::defaults':
+      location          => at,
+      apt_dater_manager => true,
+      munin_smart_disks => ['sda', 'sdb'],
+      force_nullmailer  => true;
 
     'libvirt':
     ;
