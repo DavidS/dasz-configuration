389fb7487b9e06c09305c2ae2467eebbbb4cd69c
David Schmitt <david@dasz.at>
2015-01-14 14:46:04 +0100

    fw-schmidg2: fixup network config

--- manifests/nodes/fw-schmidg2.edv-bus.at.pp
+++ manifests/nodes/fw-schmidg2.edv-bus.at.pp
@@ -1,7 +1,7 @@
 node 'fw-schmidg2.edv-bus.at' {
   class { 'dasz::defaults':
     location          => 'Schmidg',
-    primary_ip        => '192.168.0.2', # only reachable via ovpn-maria
+    primary_ip        => '192.168.0.9', # not reachable from outside
     munin_smart_disks => ['sda'],
     force_nullmailer  => true;
   }
