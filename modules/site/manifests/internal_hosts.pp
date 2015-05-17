53d2f4b35b80bf38a6763688e0135039ee25e4cd
David Schmitt <david@dasz.at>
2015-05-17 18:47:30 +0100

    site: remove old internal hosts

--- modules/site/manifests/internal_hosts.pp
+++ modules/site/manifests/internal_hosts.pp
@@ -1,27 +1,12 @@
 class site::internal_hosts {
   host {
-    'db-server':
-      ip => '10.0.0.1';
-
-    'srv2008':
-      host_aliases => ['srv2008.lan.dasz.at', 'srv2008.dasz'],
-      ip           => '10.0.0.203';
-
     # required for nginx on hetz3
     'office':
       ip => '10.0.0.221';
 
-    'jenkins':
-      host_aliases => ['jenkins.dasz'],
-      ip           => '10.0.0.216';
-
     'monitor':
       host_aliases => ['monitor.dasz'],
       ip           => '10.0.0.217';
-
-    'kvmhost':
-      host_aliases => ['kvmhost.dasz'],
-      ip           => '10.0.0.191';
 
     'fw-maria.mariatreu.at':
       host_aliases => ['fw-maria'],
